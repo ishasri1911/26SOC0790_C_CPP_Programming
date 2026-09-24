dfaa8a11153a327003a29a8c8da7d27d9d31458d
Isha Srivastava <ishasri.engg@gmail.com>
2026-09-24 22:39:46 +0530

    dev

--- src/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "26SOC0790 C/C++ Programming" << '\\n';
-    return 0;
-}
--- src/myprogram1.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main() {
-    double firstNumber, secondNumber;
-    std::cin >> firstNumber >> secondNumber;
-    std::cout << firstNumber + secondNumber;
-    return 0;
-}
